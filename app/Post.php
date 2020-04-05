0cc2e9768e4674e356f1e0dc263d106c8e95c04c
souken-b <yuto430scuderia@gmail.com>
2020-04-05 21:06:36 +0900

    postモデルのuser_id,community_id,content,picture_pathをprotected fillableに設定

--- app/Post.php
+++ app/Post.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
+    protected $fillable = ['user_id', 'community_id', 'content', 'picture_path'];
     public function user()
     {
         return $this->belongsTo('App\User');
