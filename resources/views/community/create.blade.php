2de5934b0feb796679b7be37be024f11b0daaca0
souken-b <yuto430scuderia@gmail.com>
2020-04-05 14:21:06 +0900

    communityのtypoを修正

--- resources/views/community/create.blade.php
+++ resources/views/community/create.blade.php
@@ -1,9 +1,9 @@
-@extends('layouts.comunity')
+@extends('layouts.community')
 @section('content')
-    <form method="POST" action="{{ action('ComunityController@store') }}">
+    <form method="POST" action="{{ action('CommunityController@store') }}">
         @csrf
         <input type="hidden" name="user_id" value="{{ $user->id }}">
-        <textarea name="name" placeholder="comunityのタイトルを入力"></textarea>
+        <textarea name="name" placeholder="communityのタイトルを入力"></textarea>
         <button type="submit">Submit</button>
     </form>
 @endsection
