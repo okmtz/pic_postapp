77629d9e6bc721c8e178e1e86ddef416d35f21d3
souken-b <yuto430scuderia@gmail.com>
2020-04-05 21:09:10 +0900

    imgを表示させることに成功した

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.post')
 @section('content')
     <p>{{ $post->content }}</p>
+    <img src="{{ asset('storage/pictures/'.$post->picture_path) }}" width="500" height="300">
 @endsection
