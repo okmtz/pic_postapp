4abd60a7b938b05b1baacf8abd1884c26f14c077
souken-b <yuto430scuderia@gmail.com>
2020-04-08 23:16:29 +0900

    community/index.blade.phpに削除ボタンを作成、作成したユーザーのみ削除可能にした

--- resources/views/community/index.blade.php
+++ resources/views/community/index.blade.php
@@ -4,8 +4,15 @@
 
   <a href="{{ action('CommunityController@create') }}">create</a>
 
-  @foreach( $communities as $community)
+  @foreach ( $communities as $community)
     <table>
+      @if ($user->id == $community->user_id)
+        <form method="POST" action="{{ action('CommunityController@destroy', $community) }}">
+          @csrf 
+          @method('DELETE')
+          <button type="submit">Delete</button>
+        </form>
+      @endif
       <tr>{{ $community->id }}</tr>
       <td><a href="{{ action('CommunityController@show', $community->id )}}">詳細はこちら</a></td>
       <td>{{ $community->name }}</td>
